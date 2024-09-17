efa2ea17474e17844d16b9b656e2466ce8387bd3
badubidabambirimbum <chetvergovalex@mail.ru>
2024-09-17 21:40:11 +0300

    update

--- matrix/matrixFunctions/matrixFunctions_test.go
+++ matrix/matrixFunctions/matrixFunctions_test.go
@@ -12,17 +12,18 @@ import (
 type testData[T inter.Number] struct {
 	list1 matrix.Matrix[T]
 	list2 matrix.Matrix[T]
+	val   T
 	want  matrix.Matrix[T]
 }
 
 func TestSum(t *testing.T) {
 	tests := []testData[int]{
-		testData[int]{
+		{
 			list1: CreateMatrix[int]([][]int{{1, 2, 3}, {4, 5, 6}}),
 			list2: CreateMatrix[int]([][]int{{1, 2, 3}, {4, 5, 6}}),
 			want:  CreateMatrix[int]([][]int{{2, 4, 6}, {8, 10, 12}}),
 		},
-		testData[int]{
+		{
 			list1: CreateMatrix[int]([][]int{{1, 2}, {4, 5}}),
 			list2: CreateMatrix[int]([][]int{{-2, -3}, {-10, 5}}),
 			want:  CreateMatrix[int]([][]int{{-1, -1}, {-6, 10}}),
@@ -39,12 +40,12 @@ func TestSum(t *testing.T) {
 
 func TestDifference(t *testing.T) {
 	tests := []testData[int]{
-		testData[int]{
+		{
 			list1: CreateMatrix[int]([][]int{{1, 2, 3}, {4, 5, 6}}),
 			list2: CreateMatrix[int]([][]int{{1, 2, 3}, {4, 5, 6}}),
 			want:  CreateMatrix[int]([][]int{{0, 0, 0}, {0, 0, 0}}),
 		},
-		testData[int]{
+		{
 			list1: CreateMatrix[int]([][]int{{1, 2}, {4, -5}}),
 			list2: CreateMatrix[int]([][]int{{-2, -3}, {-10, 5}}),
 			want:  CreateMatrix[int]([][]int{{3, 5}, {14, -10}}),
@@ -55,6 +56,70 @@ func TestDifference(t *testing.T) {
 		got := Difference[int](test.list1, test.list2)
 		if !reflect.DeepEqual(got.Matrix(), test.want.Matrix()) {
 			t.Errorf("Difference (%d, %d) = \"%d\", want \"%d\"", test.list1.Matrix(), test.list2.Matrix(), got.Matrix(), test.want.Matrix())
+		}
+	}
+}
+
+func TestProduct(t *testing.T) {
+	tests := []testData[int]{
+		{
+			list1: CreateMatrix[int]([][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}),
+			list2: CreateMatrix[int]([][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}),
+			want:  CreateMatrix[int]([][]int{{30, 36, 42}, {66, 81, 96}, {102, 126, 150}}),
+		},
+		{
+			list1: CreateMatrix[int]([][]int{{1, 2}, {4, -5}}),
+			list2: CreateMatrix[int]([][]int{{-2, -3}, {-10, 5}}),
+			want:  CreateMatrix[int]([][]int{{-22, 7}, {42, -37}}),
+		},
+	}
+
+	for _, test := range tests {
+		got := Product[int](test.list1, test.list2)
+		if !reflect.DeepEqual(got.Matrix(), test.want.Matrix()) {
+			t.Errorf("Product (%d, %d) = \"%d\", want \"%d\"", test.list1.Matrix(), test.list2.Matrix(), got.Matrix(), test.want.Matrix())
+		}
+	}
+}
+
+func TestMultiplicationByNumber(t *testing.T) {
+	tests := []testData[int]{
+		{
+			list1: CreateMatrix[int]([][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}),
+			val:   -5,
+			want:  CreateMatrix[int]([][]int{{-5, -10, -15}, {-20, -25, -30}, {-35, -40, -45}}),
+		},
+		{
+			list1: CreateMatrix[int]([][]int{{1, 2}, {4, -5}}),
+			val:   0,
+			want:  CreateMatrix[int]([][]int{{0, 0}, {0, 0}}),
+		},
+	}
+
+	for _, test := range tests {
+		got := MultiplicationByNumber[int](test.list1, test.val)
+		if !reflect.DeepEqual(got.Matrix(), test.want.Matrix()) {
+			t.Errorf("MultiplicationByNumbe (%d, %d) = \"%d\", want \"%d\"", test.list1.Matrix(), test.val, got.Matrix(), test.want.Matrix())
+		}
+	}
+}
+
+func TestTransposition(t *testing.T) {
+	tests := []testData[int]{
+		{
+			list1: CreateMatrix[int]([][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}),
+			want:  CreateMatrix[int]([][]int{{1, 4, 7}, {2, 5, 8}, {3, 6, 9}}),
+		},
+		{
+			list1: CreateMatrix[int]([][]int{{1, 2}, {4, -5}}),
+			want:  CreateMatrix[int]([][]int{{1, 4}, {2, -5}}),
+		},
+	}
+
+	for _, test := range tests {
+		got := Transposition[int](test.list1)
+		if !reflect.DeepEqual(got.Matrix(), test.want.Matrix()) {
+			t.Errorf("Transposition (%d) = \"%d\", want \"%d\"", test.list1.Matrix(), got.Matrix(), test.want.Matrix())
 		}
 	}
 }
